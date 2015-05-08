53346fc01574f047968ff607415042f803c19f7a
Garrett Olson <garrettdolson@gmail.com>
2015-05-08 11:47:49 -0700

    Test CRUD functionality for stylists

---
+++ spec/stylist_spec.rb
@@ -0,0 +1,105 @@
+require('spec_helper')
+
+# Note: I created a 'global' helper method called 'add_stylist' that makes
+# instantiating and saving stylists faster. Check out 'spec_helper' for details
+
+describe('Stylist') do
+  describe('#id') do
+    it('returns the stylist id') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      expect(stylist.id).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#first_name') do
+    it('returns the first name of a stylist') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      expect(stylist.first_name).to(eq('Lindsay'))
+    end
+  end
+
+  describe('#last_name') do
+    it('returns the first name of a stylist') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      expect(stylist.last_name).to(eq('Culver'))
+    end
+  end
+
+  describe('#full_name') do
+    it('returns the full name of a stylist') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      expect(stylist.full_name).to(eq('Lindsay Culver'))
+    end
+  end
+
+  describe('#save') do
+    it('will save a stylist to the database') do
+      stylist1 = Stylist.new(id: nil, first_name: 'Lindsay', last_name: 'Culver').save()
+      expect(Stylist.all()).to(eq([stylist1]))
+    end
+  end
+
+  describe('#update') do
+    it('will update a stylist first name') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      stylist.update(first_name: 'Lindz')
+      expect(Stylist.find(stylist.id).first_name).to(eq("Lindz"))
+    end
+
+    it('will update a stylist last name') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      stylist.update(last_name: 'Culvz')
+      expect(Stylist.find(stylist.id).last_name).to(eq("Culvz"))
+    end
+
+    it('will update a stylist first and last name at the same time') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      stylist.update(first_name: "Lindz", last_name: 'Culvz')
+      expect(Stylist.find(stylist.id).full_name).to(eq("Lindz Culvz"))
+    end
+  end
+
+  describe('#delete') do
+    it('will delete a specific stylist in the database') do
+      stylist1 = add_stylist('Lindsay', 'Culver')
+      stylist2 = add_stylist('Garrett', 'Olson')
+      expect(Stylist.all).to(eq([stylist1, stylist2]))
+      stylist2.delete()
+      expect(Stylist.all).to(eq([stylist1]))
+    end
+  end
+
+  describe('#find(id)') do
+    it('returns a specific stylist object out of many') do
+      stylist1 = add_stylist('Lindsay', 'Culver')
+      stylist2 = add_stylist('Garrett', 'Olson')
+      expect(Stylist.find(stylist1.id)).to(eq(stylist1))
+    end
+  end
+
+  describe('.all') do
+    it('will return an empty array when no stylists') do
+      expect(Stylist.all()).to(eq([]))
+    end
+
+    it('will return an array with a single stylist when one exists') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      expect(Stylist.all()).to(eq([stylist]))
+    end
+
+    it('will return an array of multiple stylists when multiple exist') do
+      stylist1 = add_stylist('Lindsay', 'Culver')
+      stylist2 = add_stylist('Bill', 'Brasky')
+      expect(Stylist.all()).to(eq([stylist1, stylist2]))
+    end
+  end
+
+  describe('.clear') do
+    it('will clear all stylists from the database') do
+      stylist = add_stylist('Lindsay', 'Culver')
+      expect(Stylist.all()).to(eq([stylist]))
+      Stylist.clear()
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+end
