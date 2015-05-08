ff147bf08e9d48d0169441f64b2230f3a03fea80
Garrett Olson <garrettdolson@gmail.com>
2015-05-08 13:35:57 -0700

    Add CRUD related routes for clients

--- app.rb
+++ app.rb
@@ -21,14 +21,13 @@ get('/') do
 end
 
 get('/stylists') do
-  #@stylists = Stylist.find(name: target_name)
   @stylists = Stylist.all
   erb(:stylists)
 end
 
 post('/stylists') do
-  first_name = params.fetch("first_name")
-  last_name  = params.fetch("last_name")
+  first_name = params.fetch("first_name").capitalize
+  last_name  = params.fetch("last_name").capitalize
   stylist = Stylist.new(id: nil, first_name: first_name, last_name: last_name).save()
   redirect to("/stylists/#{stylist.id}")
 end
@@ -41,4 +40,47 @@ get('/stylists/:id') do
   id = params.fetch("id").to_i
   @stylist = Stylist.find(id: id).first
   erb(:stylist)
+end
+
+patch('/stylists') do
+  id         = params.fetch("id").to_i
+  first_name = params.fetch("first_name")
+  last_name  = params.fetch("last_name")
+  stylist    = Stylist.find(id: id).first
+  stylist.update(first_name: first_name, last_name: last_name)
+  redirect back
+end
+
+delete('/stylists') do
+  id = params.fetch("id").to_i
+  stylist = Stylist.find(id: id).first
+  stylist.delete
+  redirect to('/stylists')
+end
+
+get('/clients/new') do
+  erb(:client_form)
+end
+
+post('/clients') do
+  first_name = params.fetch("first_name").capitalize
+  last_name  = params.fetch("last_name").capitalize
+  client = Client.new(id: nil, first_name: first_name, last_name: last_name).save()
+  redirect to("/clients/#{client.id}")
+end
+
+patch('/clients') do
+  id         = params.fetch("id").to_i
+  first_name = params.fetch("first_name")
+  last_name  = params.fetch("last_name")
+  client    = Client.find(id: id).first
+  client.update(first_name: first_name, last_name: last_name)
+  redirect back
+end
+
+delete('clients') do
+  id = params.fetch("id").to_i
+  client = Client.find(id: id).first
+  client.delete
+  redirect to('/clients')
 end
